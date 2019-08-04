db73a1660a75315a1ef45ab7633f4d55f84acb2a
Konstantin Ignatjev <ignatjev.konstantin@gmail.com>
2019-08-04 23:52:38 +0300

    Initial Commit

---
+++ udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  udder
+//
+//  Created by Константин Игнатьев on 8/4/19.
+//  Copyright © 2019 Константин Игнатьев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
